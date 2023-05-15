3859f4e0bd85424791dbee73c91269535901cc2b
iftekharx <iftekharx86@gmail.com>
2023-05-15 14:33:31 +0600

    Tailwind custom class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{html, js}'],
   theme: {
-    extend: {},
+    extend: {
+      'font-size': {
+        '8xl': '8rem',
+      },
+      colors: {
+        'red-custom': '#fd681b',
+      },
+    },
   },
   plugins: [],
 }
